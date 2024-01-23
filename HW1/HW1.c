af20fd014fcb33bc8e0d4cf4b52e2e2745370faa
ashtonvcole <ashtoncole1028@gmail.com>
2024-01-23 14:08:11 -0600

    Completed HW1

---
+++ HW1/HW1.c
@@ -0,0 +1,27 @@
+/*
+ * HW1.c
+ * Homework 1
+ * Ashton Cole
+ * COE 379L: Parallel Computing
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char **argv) {
+	
+	MPI_Init(&argc, &argv);
+
+	int a = 0; // Process index
+	int b = 0; // Number of processes
+	
+	MPI_Comm_rank(MPI_COMM_WORLD, &a);
+	MPI_Comm_size(MPI_COMM_WORLD, &b);
+
+	printf("Hello from process %d of %d!\n", a, b);
+
+	MPI_Finalize();
+
+	return 0;
+}
